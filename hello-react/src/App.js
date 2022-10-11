dde990c79458e72f131969ea9c09e0fd9c4c5fa7
sunay gokce ince <sgokceince@icloud.com>
2022-10-11 11:20:12 +0300

    user componentine name surname age gibi bilgiler tanımlandı.

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Form from'./component/Form.js';
+import User from './component/User.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <User 
+         name="Sakiz" 
+         surname="İnce" 
+         //isLoggedIn={true}
+         age = {3} 
+         friends ={["Gusta", "Sunay", "Orhan"]} 
+         address = {{title: "Mersin", zip: 33410}}/>
+       <h2>Kullanıcı Kayıt Formu</h2>
+       <Form />
     </div>
   );
 }
